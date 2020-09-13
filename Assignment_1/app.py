f2d6d912e37d952e06a73086386a37b1956b2390
Timothy Johansson <44690220+AsymptoticBrain@users.noreply.github.com>
2020-09-13 08:59:26 +0200

    question 1

--- Assignment_1/app.py
+++ Assignment_1/app.py
@@ -7,9 +7,18 @@ __author__ = "Timothy Johansson"
 __version__ = "0.1.0"
 __license__ = "MIT"
 
+import numpy as np
+
+DATA = 'pulses.csv'
+
 
 def main():
-    pass
+    question_1()
+
+
+def question_1():
+    data = np.loadtxt(fname=DATA, delimiter=',')
+    print(data)
 
 
 if __name__ == '__main__':
